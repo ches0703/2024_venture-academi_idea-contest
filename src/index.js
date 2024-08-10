d9d8ca15845115c4390c2260cbaf35622cb89958
ches0703 <ches0703@naver.com>
2024-08-10 10:56:48 +0900

    [Feat] : strict 모드 제거

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import router from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
+  <ThemeProvider theme={theme}>
     <CssBaseline></CssBaseline>
     <RouterProvider router={router}>
       <App />
     </RouterProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+  </ThemeProvider>
 );
