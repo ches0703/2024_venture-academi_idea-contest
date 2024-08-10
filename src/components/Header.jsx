d8a36cfc55f5438ba11141d79f5dd38e3a2570a2
ches0703 <ches0703@naver.com>
2024-08-10 10:47:27 +0900

    [Feat] : 기본 테마 설정 및 헤더 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  Box,
+  Stack,
+  Typography
+} from '@mui/material'
+import { styled } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+const JOB_SKILL = 'JOB_SKILL'
+const JOB_POSTING = 'JOB_POSTING'
 
 const Header = () => {
+
+  const navigate = useNavigate()
+  const [tab, setTab] = useState(JOB_SKILL)
+  const handleTab = (newTab) => {
+    setTab(newTab)
+  }
+
+  useEffect(() => {
+    // 탭 상태에 따라 배경 색을 변경합니다.
+    if (tab === JOB_SKILL) {
+      document.body.style.backgroundColor = '#1F2942' // JOB_SKILL에 해당하는 배경 색
+    } else if (tab === JOB_POSTING) {
+      document.body.style.backgroundColor = '#4E5A77' // JOB_POSTING에 해당하는 배경 색
+    }
+  }, [tab])
+
   return (
-    <div>Header</div>
+    <Box height={'10vh'}>
+      <Side color={'#1F2942'} left
+        select={(tab === JOB_SKILL).toString()}
+      ></Side>
+      <Side color={'#4E5A77'} right
+        select={(tab === JOB_POSTING).toString()}
+      ></Side>
+      <Stack
+        direction={'row'}
+        textAlign={'center'}
+        alignItems={'center'}
+        height={'10vh'}
+      >
+        <TabTypography
+          isSelect={(tab === JOB_SKILL)}
+          handleClick={() => {
+            navigate('/')
+            handleTab(JOB_SKILL)
+          }}
+        >
+          직무별 요구 사항
+        </TabTypography>
+        <TabTypography
+          isSelect={(tab === JOB_POSTING)}
+          handleClick={() => {
+            navigate('/job-post')
+            handleTab(JOB_POSTING)
+          }}
+        >
+          채용 공고
+        </TabTypography>
+      </Stack>
+    </Box>
+  )
+}
+
+
+const Side = styled(Box)((props) => {
+  return {
+    backgroundColor: props.color,
+    position: 'absolute',
+    width: (props.select === 'true') ? '60%' : '40%',
+    height: '10vh',
+    top: '0',
+    right: props.right && '0',
+    left: props.left && '0',
+    zIndex: (props.select === 'true') ? '-1' : '-2',
+    transition: '0.5s'
+  }
+})
+
+
+const TabTypography = ({ children, isSelect, handleClick }) => {
+  return (
+    <Typography
+      lineHeight={'10vh'}
+      fontSize={(isSelect) ? '2.4rem' : '1.8rem'}
+      flexGrow={(isSelect) ? 6 : 4}
+      onClick={handleClick}
+      color={'#FFFFFF'}
+      sx={{ transition: '0.4s ease-in-out' }}
+    >
+      {children}
+    </Typography>
   )
 }
 
--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,46 @@ import { createTheme } from "@mui/material";
 
 
 const theme = createTheme({
+  typography: {
+    fontFamily: 'Arial',
+    h1: {
+      fontSize: "2.8rem",
+      fontWeight: 700
+    },
+    h2: {
+      fontSize: "2.4rem",
+      fontWeight: 700
+    },
+    h3: {
+      fontSize: "2.0rem",
+    },
+    h4: {
+      fontSize: "1.8rem",
+    },
+    body1: {
+      fontSize: "1.6rem"
+    },
+    body2: {
+      fontSize: "1.2rem"
+    },
+    button: {
+      fontSize: "1.6rem"
+    }
+  },
+
   palette: {
+    primary: {
+      main: '#1F2942',
+      contrastText: '#FFFFFF'
+
+    },
+    secondary: {
+      main: '#4E5A77',
+      contrastText: '#FFFFFF'
+    },
+    white: {
+      main: '#FFFFFF'
+    }
   }
 })
 
