97cca06f7eded7c21bacaa3de6458b705860b5c8
ches0703 <ches0703@naver.com>
2024-08-11 14:45:05 +0900

    [Feat] :  파스텔 컬러 생성용 함수에 투면도 값 추가

--- src/utils/getPastelColor.js
+++ src/utils/getPastelColor.js
@@ -1,9 +1,12 @@
-function getPastelColor(value) {
+function getPastelColor(value, alpha) {
   const hue = 60 + (value % 180);       // 색상 : 60도 ~ 240도(노랑 ~ 파랑)
   const saturation = 80 + (value % 20); // 채도 : 50% ~ 70% 
   const lightness = 85 + (value % 10);  // 명도 : 70% ~ 80% 
-  const pastelColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-  return pastelColor;
+  if (alpha) {
+    return `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
+  } else {
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`
+  }
 }
 
 export default getPastelColor
